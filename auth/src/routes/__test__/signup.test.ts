65e3a8768b4f53de1aae530ce900496ca646223b
Pinakin Chaubal <chaubalpinakin@gmail.com>
2022-07-03 19:59:01 +0530

    working

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,23 @@ it('returns a 201 on successful signup', async ()=> {
         })
         .expect(201);
 });
+
+it('returns a 400 with an invalid email', async ()=> {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'testtestcom',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('returns a 400 with an invalid password', async ()=> {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'pas'
+        })
+        .expect(400);
+});
